5900f1651c721b357f9c45b976baeb71e7ebe951
Susan Chiemi <Susan@Users-MacBook.local>
2017-08-06 16:14:55 -0400

    new commit

---
+++ bamazonCustomer.js
@@ -0,0 +1,77 @@
+//require npm packages
+var mysql = require("mysql");
+var inquirer = require("inquirer");
+
+//connect to mysql database
+var connection = mysql.createConnection({
+    host:"localhost",
+    port:8889,
+    user:"root",
+    password:"root",
+    database:"bamazon"
+})
+
+//initialize connection
+connection.connect(function(err){
+    if (err) throw err;
+    console.log("connection successful!");
+})
+
+//make table to print to screen
+//NOT WORKING FOR ME
+var makeTable = function(){
+    connection.query("SELECT * FROM products", function(err,res){
+        for(var i=0; i<res.length; i++){
+            console.log(res[i].itemid+" || "+res[i].productname+" || "+res[i].departmentname+" || "+res[i].price+" || "+res[i].stockquantity+"\n");
+        }
+        promptCustomer(res);
+    })
+}
+    var promptCustomer = function(res){
+        inquirer.prompt([{
+            type:"input",
+            name:"choice",
+            message:"What would you like to purchase? [Quite with Q]"
+        }]).then(function(answer){
+            var correct = false;
+            if(answer.choice.toUpperCase()=="Q"){
+                process.exist();
+            }
+
+
+            for(var i=0;i<res.length;i++){
+                if(res[i].productname==answer.choice){
+                    correct=true;
+                    var product=answer.choice;
+                    var id=i;
+                    inquirer.prompt({
+                        type:"input",
+                        name:"quant",
+                        message:"How much would you like to buy",
+                        validate: function(value){
+                            if(isNan(value)==false){
+                                return true;
+                            } else {
+                                return false;
+                            }
+                        }
+                    }).then(function(answer){
+                        if((res[id].stockquanity-answer.quant)>0){
+                            connection.query("UPDATE products SET stockquantity='"+(res[id].stockquantity-answer.quant)+"' WHERE productname='"+product+"'", function(err,res2){
+                                console.log("Product Bought!");
+                                makeTable();
+                            })
+                        }else {
+                            console.log("Not a valid selection!");
+                            promptCustomer(res);
+                        }
+                    })
+                }
+            }
+            if(i==res.length && correct==false){
+                console.log("Not a valid selection!");
+                promptCustomer(res);
+            }
+        })
+
+    }
---
+++ schema.sql
@@ -0,0 +1,19 @@
+create database bamazon;
+use bamazon;
+
+create table products (
+	itemid integer auto_increment not null,
+	productname varchar(45) not null,
+	departmentname varchar(45) not null,
+	price decimal(10, 4) not null,
+	stockquantity integer(10) not null,
+	primary key (itemid)
+);
+
+INSERT INTO products(ProductName,DepartmentName,Price,StockQuantity)
+VALUES ("Uncharted 4", "VideoGames",49.95,150),
+	("Doom","Video Games",59.99,200),
+	("Crate of Spam","Food and Drink",24.50,50);
+	
+SELECT * FROM bamazon.products;
+	
